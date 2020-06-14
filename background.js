f44735f77a2766048fe6b9c9e077f4251e5119c6
zjyeung <zjyeung579@gmail.com>
2020-06-13 20:24:06 -0700

    Redirect to timer.html

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         console.log('In webRequest');
-        return {cancel: true};
+        return {
+            redirectUrl: chrome.extension.getURL('timer.html') 
+        };
     }, {
         urls: ["https://www.youtube.com/*", "https://www.facebook.com/*"]
     }, ["blocking"]
