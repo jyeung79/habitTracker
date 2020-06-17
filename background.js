423ad00521e0ca77a01544b2ed4b7832ac8962de
zjyeung <zjyeung579@gmail.com>
2020-06-16 21:20:21 -0700

    Remove deprecated .extension with .runtime

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         console.log('In webRequest');
         return {
-            redirectUrl: chrome.extension.getURL('timer.html') 
+            redirectUrl: chrome.runtime.getURL('timer.html') 
         };
     }, {
         urls: ["https://www.youtube.com/*", "https://www.facebook.com/*"]
